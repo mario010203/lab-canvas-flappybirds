8bc193c1f8da48d0141f3202ff90f6f82cf7a21b
mario010203 <mariojose1490@hotmail.com>
2019-12-07 11:48:20 +0100

    Complete iteration 1

--- starter_code/JS/background.js
+++ starter_code/JS/background.js
@@ -1,17 +1,33 @@
-class Background{
-constructor(ctx,width, height){
-    this.ctx=ctx
-    this.width=width
-    this.height=height
-    this.image=new Image();
-    this.image.src="starter_code/images/bg.png";
-    this.posX=0;
-    this.posy=0;
-    //this.vx=8; background speed
-}
-draw(){
-    this.ctx.drawImage
-}
+class Background {
+    constructor(ctx, width, height) {
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+        this.image = new Image();
+        this.image.src = "starter_code/images/bg.png";
+        this.posX = 0;
+        this.posy = 0;
+        //this.vx=8; background speed
+    }
+    draw() {
+        this.ctx.drawImage(
+            this.image,
+            this.posX,
+            this.posY,
+            this.width,
+            this.height
+        );
+        this.ctx.drawImage(
+            this.image,
+            this.posX + this.width,
+            this.posY,
+            this.width,
+            this.height
+        );
+    }
 
-
+    move() {
+        this.posX -= this.vx;
+        if (this.posX <= -this.width) this.posX = 0;
+    }
 }
