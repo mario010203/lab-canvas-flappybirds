30b92b532ce7a35a6aeacb32f6cf4ecd393c2fe6
mario010203 <mariojose1490@hotmail.com>
2019-12-07 11:41:16 +0100

    background and method

---
+++ starter_code/JS/background.js
@@ -0,0 +1,17 @@
+class Background{
+constructor(ctx,width, height){
+    this.ctx=ctx
+    this.width=width
+    this.height=height
+    this.image=new Image();
+    this.image.src="starter_code/images/bg.png";
+    this.posX=0;
+    this.posy=0;
+    //this.vx=8; background speed
+}
+draw(){
+    this.ctx.drawImage
+}
+
+
+}
